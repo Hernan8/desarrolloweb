4dfbde74c3fbed8ef249c79e5cbece89e00463d9
HernanCortez <nolohaga1@hotmail.com>
2017-05-06 14:16:13 -0600

    Actualize las rutas hacia mis archivos css y le inserte codigo javascript, puse 5 radio button para cada seccion de mi vida en la biografia, y reajuste el texto e imagenes dandole un valor estatico

---
+++ js/biografia.js
@@ -0,0 +1,188 @@
+function onload() { 
+document.getElementById("radio1").style.display = "block";
+document.getElementById("radio2").style.display = "none";
+document.getElementById("radio3").style.display = "none";
+document.getElementById("radio4").style.display = "none";
+document.getElementById("radio5").style.display = "none";
+document.getElementById("radio6").style.display = "none";
+
+}
+ 
+
+
+
+
+function mostrarcomienzo(){
+element = document.getElementById("radio1");
+        check = document.getElementById("uno");
+	element2 = document.getElementById("radio2");
+	element3 = document.getElementById("radio3");
+	element4 = document.getElementById("radio4");
+	element5 = document.getElementById("radio5");
+	element6 = document.getElementById("radio6");
+        if (check.checked) {
+            element.style.display='block';
+	    element2.style.display='none';
+	    element3.style.display='none';
+	    element4.style.display='none';
+	    element5.style.display='none';
+	    element6.style.display='none';
+        }
+        else {
+            element.style.display='block';
+	    element2.style.display='none';
+	    element3.style.display='none';
+	    element4.style.display='none';
+	    element5.style.display='none';
+	    element6.style.display='none';
+        }
+
+}
+
+function mostrarkinder(){
+element = document.getElementById("radio2");
+        check = document.getElementById("dos");
+	element2 = document.getElementById("radio1");
+	element3 = document.getElementById("radio3");
+	element4 = document.getElementById("radio4");
+	element5 = document.getElementById("radio5");
+	element6 = document.getElementById("radio6");
+        if (check.checked) {
+            element.style.display='block';
+	    element2.style.display='none';
+	    element3.style.display='none';
+	    element4.style.display='none';
+	    element5.style.display='none';
+	    element6.style.display='none';
+        }
+        else {
+            element.style.display='block';
+	    element2.style.display='none';
+	    element3.style.display='none';
+	    element4.style.display='none';
+	    element5.style.display='none';
+	    element6.style.display='none';
+        }
+
+
+
+
+}
+function mostrarprimaria(){
+element = document.getElementById("radio3");
+        check = document.getElementById("tres");
+	element2 = document.getElementById("radio1");
+	element3 = document.getElementById("radio2");
+	element4 = document.getElementById("radio4");
+	element5 = document.getElementById("radio5");
+	element6 = document.getElementById("radio6");
+        if (check.checked) {
+            element.style.display='block';
+	    element2.style.display='none';
+	    element3.style.display='none';
+	    element4.style.display='none';
+	    element5.style.display='none';
+	    element6.style.display='none';
+        }
+        else {
+            element.style.display='block';
+	    element2.style.display='none';
+	    element3.style.display='none';
+	    element4.style.display='none';
+	    element5.style.display='none';
+	    element6.style.display='none';
+        }
+
+
+
+
+
+}
+function mostrarsecundaria(){
+element = document.getElementById("radio4");
+        check = document.getElementById("cuatro");
+	element2 = document.getElementById("radio1");
+	element3 = document.getElementById("radio2");
+	element4 = document.getElementById("radio3");
+	element5 = document.getElementById("radio5");
+	element6 = document.getElementById("radio6");
+        if (check.checked) {
+            element.style.display='block';
+	    element2.style.display='none';
+	    element3.style.display='none';
+	    element4.style.display='none';
+	    element5.style.display='none';
+	    element6.style.display='none';
+        }
+        else {
+            element.style.display='block';
+	    element2.style.display='none';
+	    element3.style.display='none';
+	    element4.style.display='none';
+	    element5.style.display='none';
+	    element6.style.display='none';
+        }
+
+
+
+
+
+}
+function mostrarpreparatoria(){
+element = document.getElementById("radio5");
+        check = document.getElementById("cinco");
+	element2 = document.getElementById("radio1");
+	element3 = document.getElementById("radio2");
+	element4 = document.getElementById("radio3");
+	element5 = document.getElementById("radio4");
+	element6 = document.getElementById("radio6");
+        if (check.checked) {
+            element.style.display='block';
+	    element2.style.display='none';
+	    element3.style.display='none';
+	    element4.style.display='none';
+	    element5.style.display='none';
+	    element6.style.display='none';
+        }
+        else {
+            element.style.display='block';
+	    element2.style.display='none';
+	    element3.style.display='none';
+	    element4.style.display='none';
+	    element5.style.display='none';
+	    element6.style.display='none';
+        }
+
+
+
+
+
+
+}
+function mostraruniversidad(){
+	element = document.getElementById("radio6");
+        check = document.getElementById("seis");
+	element2 = document.getElementById("radio1");
+	element3 = document.getElementById("radio2");
+	element4 = document.getElementById("radio3");
+	element5 = document.getElementById("radio4");
+	element6 = document.getElementById("radio5");
+        if (check.checked) {
+            element.style.display='block';
+	    element2.style.display='none';
+	    element3.style.display='none';
+	    element4.style.display='none';
+	    element5.style.display='none';
+	    element6.style.display='none';
+        }
+        else {
+            element.style.display='block';
+	    element2.style.display='none';
+	    element3.style.display='none';
+	    element4.style.display='none';
+	    element5.style.display='none';
+	    element6.style.display='none';
+        }
+
+
+}
